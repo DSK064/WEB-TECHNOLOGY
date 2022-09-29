7c296fb1f00c81787a5d73cd7ef92fa33dfa0489
DSK064 <85743002+DSK064@users.noreply.github.com>
2022-09-29 10:38:00 +0530

    Add files via upload

--- Boomerang.js
+++ Boomerang.js
@@ -1,25 +1,24 @@
-let myArray1 = [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2];
-const size1 = myArray1.length;
-
-let myArray2 = [9, 5, 9, 5, 1, 1, 1];
-const size2 = myArray2.length;
+let myArray = [9,5,9,5,1,1,1];
+const size = myArray.length;
 
 
-console.log(countBoomerangs(myArray1, size1));
-console.log(countBoomerangs(myArray2, size2));
+// 3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2
+// 5,6,6,7,6,3,9
+// 4,4,4,9,9,9,9
+// 9,5,9,5,1,1,1
+
+
+console.log(countBoomerangs(myArray, size));
 
 function countBoomerangs(myArray, size) 
 {
     let count = 0;
-    for(let i = 0; i < size - 1; i++)
+    for(let i = 0; i + 2 <= size - 1; i++)
     {
-        if(i + 2 <= size-1)
+        let nextIndex = i + 2;
+        if((myArray[i] == myArray[nextIndex]) && (myArray[i] != myArray[i+1]))
         {
-            let nextIndex = i + 2;
-            if(myArray[i] == myArray[nextIndex])
-            {
                 count++;
-            }
         }
     }
     return count;
